8bd96221106c103adf493cf49afcde314804c834
Jace Roldan <jaceroldan16@gmail.com>
2022-10-08 17:10:31 +0800

    Enable waving of grid by a factor of frame times 10

--- sketch-noise.js
+++ sketch-noise.js
@@ -1,12 +1,14 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080],
+  animate: true,
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
@@ -30,8 +32,9 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
-      const n = random.noise2D(x, y);
-      const angle = n * Math.PI;
+      const n = random.noise2D(x + frame * 10, y, 0.001);
+      const angle = n * Math.PI * 0.2;
+      const scale = math.mapRange(n, -1, 1, 1, 30);
 
       context.save();
       context.translate(x, y);
@@ -39,7 +42,7 @@ const sketch = () => {
       context.translate(cellw * 0.5, cellh * 0.5);
       context.rotate(angle);
 
-      context.lineWidth = 4;
+      context.lineWidth = scale;
       context.beginPath();
       context.moveTo(w * -0.5, 0);
       context.lineTo(w * 0.5, 0);
