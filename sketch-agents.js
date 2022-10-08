5035a4ed18ac3b2bfcbff62612335d71e6d600a4
Jace Roldan <jaceroldan16@gmail.com>
2022-10-08 15:47:21 +0800

    Enable moving circular agents
    With bounce.

--- sketch-agents.js
+++ sketch-agents.js
@@ -1,34 +1,69 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080],
+  animate: true,
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+  const agents = [];
+  for (let i = 0; i < 40; i++) {
+      const x = random.range(0, width);
+      const y = random.range(0, height);
+
+      agents.push(new Agent(x, y));
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const pointA = new Point(800, 400, 10);
-    const pointB = new Point(300, 700, 10);
-
-    context.beginPath();
-    context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
-    context.fill();
-
-    context.beginPath();
-    context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
-    context.fill();
+    agents.forEach(agent => {
+      agent.draw(context);
+      agent.update();
+      agent.bounce(width, height);
+    });
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
-  constructor(x, y, radius) {
+class Vector {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Vector(x, y);
+    this.velocity = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
+  }
+
+  update() {
+    this.pos.x += this.velocity.x;
+    this.pos.y += this.velocity.y;
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.velocity.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.velocity.y *= -1;
+  }
+
+  draw(context) {
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
+
+    context.beginPath();
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
